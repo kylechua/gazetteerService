ef4239e0f0ab9edd990ab18e5f1e3b10a183cf17
Alexander Kyle Chua <kyle.chua16@gmail.com>
2017-08-08 16:23:49 -0700

    Base functionality (getting list of countries, children), undocumented, and unoptimized for cities/states/etc...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http')
+var URL = require('url')
+var path = require('path')
 var sqlite3 = require('sqlite3')
 
 var gazetteer = require('./src/gazetteer.js')
@@ -19,7 +21,11 @@ function init() {
     setup = gazetteer.loadData();
     setup.then(res => {
         server.listen(opts.PORT, function callback(err) {
-            console.log("Listening on " + JSON.stringify(server.address()))
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Gazetteer listening on " + JSON.stringify(server.address()));
+            }   
         });
     });
     setup.catch(err => {
@@ -27,18 +33,39 @@ function init() {
     });
 }
 
-
 server.on('request', function(request, response) {
-    response.write("Test")
-    response.end()
+    var myPath = path.parse(request.url);
+    var pathStr = path.format(myPath);
+    if (pathStr.indexOf(opts.PREFIX + 'currency/rates') != -1) {
+        console.log("currency service")
+    } else if (pathStr.indexOf(opts.PREFIX + 'gazetteer/') != -1 ) {
+        if (pathStr.indexOf('locations') != -1) {
+            server.emit('locations', request, response);
+        }
+    } else {
+        // Not found, wrong URI
+        // response.statusCode = 404;
+    }
+    response.end();
 });
 
-/*
-Object.keys(countries).forEach(country => {
-    if (countries[country].altnames.hasOwnProperty('ko')) {
-        console.log(countries[country].altnames['ko'])
-    } else {
-    console.log(countries[country].name)
+server.on('locations', function (request, response, db) {
+    var url = URL.parse(request.url, true).path;
+    var querystr = url.split('?')[1];
+    if (querystr != undefined){
+        var queries = querystr.split('&');
+        var query = {};
+        queries.forEach(param => {
+            var temp = param.split('=');
+            var key = temp[0];
+            var val = temp[1];
+            query[key] = val;
+        })
+        data = gazetteer.getLocations(query, opts.defaultlang);
+        data.then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
     }
 });
-*/
--- src/gazetteer.js
+++ src/gazetteer.js
@@ -1,16 +1,20 @@
 var sqlite3 = require('sqlite3')
 var utils = require('./utils.js')
+var fs = require('fs'), readline = require('readline')
+
 
 /* Load the required data */
 exports.loadData = function() {
     return new Promise(function(resolve, reject) {
-        global.DB = new sqlite3.Database(__dirname + '/../data/geonames_data.sql', sqlite3.OPEN_READONLY, function callback(err) {
+        global.DB = new sqlite3.Database(__dirname + '/../data/geonames_data.db', function callback(err) {
             if (err) {
-                throw(err)
+                throw(err);
             } else {
-                console.log("Gazetteer database loaded.")
+                console.log("Database loaded from " + __dirname + '/../data/geonames_data.db')
             }
+            
         });
+        
         parser = utils.parseJsonAsync(__dirname + '/../data/countryList.json');
         parser.then(json => {
             global.COUNTRIES = json;
@@ -26,5 +30,125 @@ exports.loadData = function() {
             console.log(err.message)
             reject(err)
         });
+
     });
+}
+
+exports.getLocations = function(request, defaultlang) {
+
+    return new Promise(function(resolve, reject) {
+        var response = {};
+        var lang;
+        if (request.hasOwnProperty('lang')) {
+            lang = request['lang'];
+        } else {
+            lang = defaultlang;
+        }
+        // Check if ID is given
+        if (request.hasOwnProperty('id') && request.hasOwnProperty('country')) {
+            var id = request['id'];
+            var code = request['country'];
+            getChildren(id, code, lang).then(res => {
+                response['data'] = res;
+                resolve(response);
+            }).catch(err => {
+                response['msg'] = err;
+                resolve(response);
+            });
+        } else { // No ID, return country list
+            var countries = getCountries(lang);
+            response['data'] = countries;
+            response['msg'] = "No ID or country code specified, returned the list of countries in language: " + lang;
+            resolve(response);
+        }
+    })
+}
+
+function getCountries(lang) {
+
+    var data = [];
+    Object.keys(COUNTRIES).forEach(key => {
+            var country = COUNTRIES[key];
+            var code = country.codes;
+            var id = country.id;
+            var name;
+            var key = key.toString();
+            if (country.hasOwnProperty('altnames')) {
+                if (lang != 'en' && country.altnames.hasOwnProperty(lang)) {
+                    name = country.altnames[lang];
+                } else {
+                    name = key;
+                }
+            }
+            var info = {};
+            info['key'] = key;
+            info['name'] = name;
+            info['code'] = code;
+            info['id'] = id;
+            data.push(info);
+        });
+    return data;
+}
+
+function getChildren(id, code, lang) {
+
+    return new Promise(function(resolve, reject) {
+        var tableName = 'country' + code.toUpperCase();
+        // Check if country code is valid
+        var query = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "'";
+        DB.get(query, function callback(err, res) {
+            if (err) {
+                reject(err)
+            }
+            if (res) {
+                var childrenList = HIERARCHY[id];
+                var num = childrenList.length;
+                var data = [];
+                var promises = [];
+                var last = false;
+                childrenList.forEach(function(child, index) {
+                        var command = "SELECT * FROM " + tableName + " WHERE id=" + child;
+                        promises.push(getChild(command, lang, code))
+                    });
+                Promise.all(promises).then(values => {
+                    // Remove undefined values (e.g. U.S. Virgin Islands)
+                    resolve(values.filter(Boolean))
+                });
+            } else {
+                reject("Invalid country code");
+            }
+        });
+    });
+}
+
+function getChild(command, lang, code) {
+
+    return new Promise(function(resolve, reject) {
+        DB.get(command, function callback(err, child) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (child != undefined) {
+                    var id = child.id;
+                    var name;
+                    var altnames = JSON.parse(child.altnames);
+                    if (lang != 'en' && altnames.hasOwnProperty(lang)){
+                        name = altnames[lang];
+                    } else {
+                        name = child.name;
+                    }
+                    var info = {};
+                    info['key'] = child.name;
+                    info['name'] = name;
+                    info['code'] = code;
+                    info['id'] = id;
+                    resolve(info);
+                } else {
+                    // This is probably a dependent state (e.g. U.S. Virgin Islands)
+                    // So we don't add it to the data (it should appear as a country)
+                    resolve();
+                }
+            }
+        });
+    })
 }
